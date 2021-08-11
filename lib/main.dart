950b92361ddd87b1466d5743febf3fb71e58c581
rajukumar <rajupraaa1234@gmail.com>
2021-08-11 19:20:46 +0530

    added login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,226 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
+import 'dart:developer';
 
 void main(){
-  runApp(Calculator());
+  runApp(LoginScreen());
 }
 
-class Calculator extends StatelessWidget {
+
+class LoginScreen extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Calculator',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: SimpleCalculator(),
-    );
-  }
-}
-
-class SimpleCalculator extends StatefulWidget {
-  @override
-  _SimpleCalculatorState createState() => _SimpleCalculatorState();
-}
-
-class _SimpleCalculatorState extends State<SimpleCalculator> {
-
-  String equation = "0";
-  String result = "0";
-  String expression = "";
-  double equationFontSize = 38.0;
-  double resultFontSize = 48.0;
-
-  buttonPressed(String buttonText){
-    setState(() {
-      if(buttonText == "C"){
-        equation = "0";
-        result = "0";
-        equationFontSize = 38.0;
-        resultFontSize = 48.0;
-      }
-
-      else if(buttonText == "⌫"){
-        equationFontSize = 48.0;
-        resultFontSize = 38.0;
-        equation = equation.substring(0, equation.length - 1);
-        if(equation == ""){
-          equation = "0";
-        }
-      }
-
-      else if(buttonText == "="){
-        equationFontSize = 38.0;
-        resultFontSize = 48.0;
-
-        expression = equation;
-        expression = expression.replaceAll('×', '*');
-        expression = expression.replaceAll('÷', '/');
-
-        try{
-          Parser p = Parser();
-          Expression exp = p.parse(expression);
-
-          ContextModel cm = ContextModel();
-          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
-        }catch(e){
-          result = "Error";
-        }
-
-      }
-
-      else{
-        equationFontSize = 48.0;
-        resultFontSize = 38.0;
-        if(equation == "0"){
-          equation = buttonText;
-        }else {
-          equation = equation + buttonText;
-        }
-      }
-    });
-  }
-
-  Widget buildButton(String buttonText, double buttonHeight, Color buttonColor){
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
-      color: buttonColor,
-      child: FlatButton(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(0.0),
-              side: BorderSide(
-                  color: Colors.white,
-                  width: 1,
-                  style: BorderStyle.solid
-              )
-          ),
-          padding: EdgeInsets.all(16.0),
-          onPressed: () => buttonPressed(buttonText),
-          child: Text(
-            buttonText,
-            style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.normal,
-                color: Colors.white
-            ),
-          )
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(title: 'Flutter Login'),
     );
   }
+}
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
 
   @override
   Widget build(BuildContext context) {
+    final emailField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final loginButon = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
     return Scaffold(
-      appBar: AppBar(title: Text('Simple Calculator')),
-      body: Column(
-        children: <Widget>[
-
-
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
-            child: Text(equation, style: TextStyle(fontSize: equationFontSize),),
-          ),
-
-
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
-            child: Text(result, style: TextStyle(fontSize: resultFontSize),),
-          ),
-
-
-          Expanded(
-            child: Divider(),
-          ),
-
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: MediaQuery.of(context).size.width * .75,
-                child: Table(
-                  children: [
-                    TableRow(
-                        children: [
-                          buildButton("C", 1, Colors.redAccent),
-                          buildButton("⌫", 1, Colors.blue),
-                          buildButton("÷", 1, Colors.blue),
-                        ]
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 155.0,
+                      child: Image.asset(
+                        'asset/images/calc.webp',
+                        fit: BoxFit.contain,
+                      ),
                     ),
-
-                    TableRow(
-                        children: [
-                          buildButton("7", 1, Colors.black54),
-                          buildButton("8", 1, Colors.black54),
-                          buildButton("9", 1, Colors.black54),
-                        ]
+                    SizedBox(height: 45.0),
+                    emailField,
+                    SizedBox(height: 25.0),
+                    passwordField,
+                    SizedBox(
+                      height: 35.0,
                     ),
-
-                    TableRow(
-                        children: [
-                          buildButton("4", 1, Colors.black54),
-                          buildButton("5", 1, Colors.black54),
-                          buildButton("6", 1, Colors.black54),
-                        ]
-                    ),
-
-                    TableRow(
-                        children: [
-                          buildButton("1", 1, Colors.black54),
-                          buildButton("2", 1, Colors.black54),
-                          buildButton("3", 1, Colors.black54),
-                        ]
-                    ),
-
-                    TableRow(
-                        children: [
-                          buildButton(".", 1, Colors.black54),
-                          buildButton("0", 1, Colors.black54),
-                          buildButton("00", 1, Colors.black54),
-                        ]
+                    loginButon,
+                    SizedBox(
+                      height: 15.0,
                     ),
                   ],
                 ),
               ),
-
-
-              Container(
-                width: MediaQuery.of(context).size.width * 0.25,
-                child: Table(
-                  children: [
-                    TableRow(
-                        children: [
-                          buildButton("×", 1, Colors.blue),
-                        ]
-                    ),
-
-                    TableRow(
-                        children: [
-                          buildButton("-", 1, Colors.blue),
-                        ]
-                    ),
-
-                    TableRow(
-                        children: [
-                          buildButton("+", 1, Colors.blue),
-                        ]
-                    ),
-
-                    TableRow(
-                        children: [
-                          buildButton("=", 2, Colors.redAccent),
-                        ]
-                    ),
-                  ],
-                ),
-              )
-            ],
+            ),
           ),
-
-        ],
-      ),
+        )
     );
   }
 }
+
